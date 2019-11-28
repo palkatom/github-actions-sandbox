ba7c75e6140e34da364ac9e52d855037e8d49b98
palkatom <38882604+palkatom@users.noreply.github.com>
2019-11-28 15:59:30 +0100

    Update index.js

--- .github/actions/pr/index.js
+++ .github/actions/pr/index.js
@@ -4,7 +4,8 @@ const github = require('@actions/github');
 try {
 	const g = JSON.stringify(github.context, undefined, 2);
 	console.log(`github context: ${g}`);
-
+        const go = JSON.stringify(github, undefined, 2);
+        console.log(`github object: ${go}`);
 } catch (error) {
 	core.setFailed(error.message);
 }
